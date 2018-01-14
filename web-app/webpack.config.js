a90909c166d716bd52b589197d87fc279485f967
szemmouri <s.zemmouri@proway-fs.com>
2018-01-14 15:25:05 +0100

    setup webpack, flow, eslint

---
+++ web-app/webpack.config.js
@@ -0,0 +1,50 @@
+module.exports = {
+    entry: [
+        //'webpack-dev-server/client?http://localhost:8080',
+        './src/index.js'
+    ],
+    module: {
+        rules: [
+            {
+                test: /.jsx?$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                include: [ __dirname ],
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            },
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: 'babel-loader'
+                    }
+                ]
+            },
+            { test: /\.css$/, loader: "style-loader!css-loader" },
+            {
+                test: /\.svg/,
+                use: {
+                    loader: 'svg-url-loader',
+                    options: {}
+                }
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['*', '.js', '.jsx']
+    },
+    output: {
+        path: __dirname + '/dist',
+
+        filename: 'bundle.js'
+    },
+    devServer: {
+        contentBase: './dist',
+        historyApiFallback: true
+    },
+    devtool: '#inline-source-map'
+
+};
