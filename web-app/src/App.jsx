60ba89f6d4f5f5961a480863ce670bfcaa696fc7
szemmouri <s.zemmouri@proway-fs.com>
2018-01-20 12:11:45 +0100

    create index html and start App

---
+++ web-app/src/App.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+
+    render() {
+        return (
+            <div>
+                main
+            </div>
+        )
+    }
+}
+
+export default App;
