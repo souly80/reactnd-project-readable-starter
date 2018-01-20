fe9849ed9699411daec8c898a13b8a2a98175c57
szemmouri <s.zemmouri@proway-fs.com>
2018-01-20 14:59:50 +0100

    install redux

--- web-app/src/index.jsx
+++ web-app/src/index.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App';
 import {BrowserRouter} from "react-router-dom";
+import {Provider} from "react-redux";
 
 ReactDOM.render(
-    <BrowserRouter basename=""><App /></BrowserRouter>, document.getElementById('root'))
+    <Provider>
+        <BrowserRouter basename="">
+            <App />
+        </BrowserRouter>
+    </Provider>, document.getElementById('root'))
