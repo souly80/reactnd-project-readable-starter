820cc35e9f34bb7a9304f6bf3680c59465188987
szemmouri <s.zemmouri@proway-fs.com>
2018-01-20 14:23:17 +0100

    create API to list categories

--- web-app/src/main.jsx
+++ web-app/src/main.jsx
@@ -1,14 +1,31 @@
 // @flow
 
 import * as React from "react";
-import {Link, Route, Switch} from "react-router-dom";
-import {Category} from "./category";
-import {Modify} from "./modify";
+import * as API from './services/api';
 
 export class Main extends React.PureComponent {
+    constructor(props) {
+        super(props);
+        this.state = {categories: []};
+        this.getCategories();
+    }
+    getCategories() {
+        API.getCategories().then((categories) => {
+            this.setState({categories});
+        })
+    }
+
+    renderCategories = () => {
+        let retValues: any[] = [];
+        if(this.state.categories.length !== 0) {
+            this.state.categories.map((category) => {
+                retValues.push(<div>{category.name}</div>);
+            });
+        }
+        return retValues;
+    }
+
     render() {
-        return (
-            <div>main</div>
-        );
+        return <div>{this.renderCategories()}</div>;
     }
 }
