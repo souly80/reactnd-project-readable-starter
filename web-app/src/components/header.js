832ffbb55f7129227cea1fbae618cf6c911f7cc3
szemmouri <s.zemmouri@proway-fs.com>
2018-02-04 17:59:50 +0100

    Stateless functional component

--- web-app/src/components/header.js
+++ web-app/src/components/header.js
@@ -1,20 +1,17 @@
 import React, {Component} from 'react';
 import {Link} from "react-router-dom";
 
-class Header extends Component {
-
-  render() {
-    const {categories} = this.props;
+const Header = ({categories}) => {
     return (
         <ul>
-          <li><Link to='/'><span>home</span></Link></li>
+            <li><Link to='/'><span>home</span></Link></li>
             {categories && categories.map((category) => (
                 <li><Link to={category.name}><span>{category.name}</span></Link></li>
             ))}
-          <li style={{float:'right'}}><Link to='/newpost' ><span>Add Post</span></Link></li>
+            <li style={{float:'right'}}><Link to='/newpost' ><span>Add Post</span></Link></li>
         </ul>
     );
-  };
-}
+};
+
 
 export default Header;
